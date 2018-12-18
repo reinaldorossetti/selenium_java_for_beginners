4d2dcd49b476dd5538b86afdef5d2e4b2e0bda8a
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-12-18 11:15:25 -0200

    Update ConcretePage.java
    Removendo validação desnecessária, melhorando o css em caso de o B de busca ser maiúsculo ou minusculo.

--- src/test/java/br/com/selenium/for_beginners/pages/ConcretePage.java
+++ src/test/java/br/com/selenium/for_beginners/pages/ConcretePage.java
@@ -6,7 +6,7 @@ import br.com.selenium.for_beginners.utils.HandleProperties;
 
 public class ConcretePage extends Page {
 
-	private static final By INPUT_SEARCH = By.cssSelector("input[placeholder='faça sua busca...']");
+	private static final By INPUT_SEARCH = By.cssSelector("form input[type=text][placeholder*='usca']");
 	private static final By BUTTON_SEARCH = By.cssSelector("span[class='icon-search']");
 
 	public void visit(String url) {
@@ -23,8 +23,8 @@ public class ConcretePage extends Page {
 	}
 
 	public boolean verifySearch(String message) {
-		By MESSAGE = By.xpath("//div[@class='_byCx']//h3//div[contains(.,'" + message + "')]");
-		isElementAttachedToHtml(MESSAGE);
-		return getTextFromLabel(MESSAGE).equals(message);
+		System.out.println(message);
+		By MESSAGE = By.xpath("//div[contains(.,'" + message + "')]");
+		return isElementAttachedToHtml(MESSAGE);
 	}
 }
