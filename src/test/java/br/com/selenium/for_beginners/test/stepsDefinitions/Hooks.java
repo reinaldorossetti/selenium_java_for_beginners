cc51856a1385c1083d0a9ffd2186f0727757dee7
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-12-28 15:41:03 -0200

    Update Hooks.java

--- src/test/java/br/com/selenium/for_beginners/test/stepsDefinitions/Hooks.java
+++ src/test/java/br/com/selenium/for_beginners/test/stepsDefinitions/Hooks.java
@@ -14,7 +14,7 @@ public class Hooks {
     public void afterEachScenario(Scenario scenario) {
 	//if (scenario.isFailed()) {
 	final byte[] screenshot = ((TakesScreenshot) Browsers.driver()).getScreenshotAs(OutputType.BYTES);
-	scenario.embed(screenshot, "image/png", "click here!");
+	scenario.embed(screenshot, "image/png");
 	//}
         AllureHelper.saveScreenshootOfScenario(scenario);
         Browsers.quitDriver();
