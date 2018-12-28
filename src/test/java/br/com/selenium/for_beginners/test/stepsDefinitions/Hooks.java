b118734874a764d5cac16837b0df54a349dee727
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-12-28 15:14:50 -0200

    Update Hooks.java

--- src/test/java/br/com/selenium/for_beginners/test/stepsDefinitions/Hooks.java
+++ src/test/java/br/com/selenium/for_beginners/test/stepsDefinitions/Hooks.java
@@ -4,11 +4,19 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import br.com.selenium.for_beginners.enums.Browsers;
 import br.com.selenium.for_beginners.utils.AllureHelper;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
     @After
     public void afterEachScenario(Scenario scenario) {
+		if (scenario.isFailed()) {
+			final byte[] screenshot = ((TakesScreenshot) Browsers.driver()).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png");
+			}
         AllureHelper.saveScreenshootOfScenario(scenario);
         Browsers.quitDriver();
     }
